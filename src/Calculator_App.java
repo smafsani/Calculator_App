b3e8d947e17c3d2e01055db58a72066e6dcaf115
smafsani <68096636+smafsani@users.noreply.github.com>
2020-09-20 00:32:26 +0600

    Initial commit

---
+++ src/Calculator_App.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Calculator_App.java
+ *
+ * Calculator_App application
+ *
+ * @author 
+ * @version 1.00 2020/6/27
+ */
+ 
+public class Calculator_App {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
